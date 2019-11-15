ab8c0dd61ef64158695b580809eacd29c87fb89a
Nazrul Babu <n95babu@gmail.com>
2019-11-15 17:25:16 -0500

    Established Api Connections

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,32 @@ export default class Input extends Component {
   render() {
     return (
       <div className="home">
+        <div>
+          <div className='Randinput'>
+            <form onSubmit={
+              this.props.handleSubmit
+            }>
+              <input className="input"
+                value={this.props.userInput}
+                onChange={this.props.handleChange}
+                placeholder="Favorite Song"
+              />&nbsp;
+              <button className="btn btn-dark" type="submit">Search</button>
+            </form>
+          </div>
+          <div className="scrollable-area">
+            {this.props.songs.map(song => (
+              <div className="rounded-pill">
+                <div className="scrollable">
+                  <h3>{song.artistName}</h3>
+                  <p>{song.trackCensoredName}</p>
+                  <p>{song.collectionViewUrl}</p>
 
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     )
   }
--- src/services/api-helper.js
+++ src/services/api-helper.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 // const BaseURL = ``;
 
 export const randomSongs = async (song) => {
-  const response = await axios.get(`https://itunes.apple.com/search?term=${song}&limit=25.`);
-  const data = response.data;
+  const response = await axios.get(`https://itunes.apple.com/search?term=senorita&limit=3.`);
+  const data = response.data.results;
   console.log(data);
   return data;
 }
