d19ed1d3d2b574c3fe6705660be95045452fd6d9
Nazrul Babu <n95babu@gmail.com>
2019-11-16 14:40:19 -0500

    Fix UI

--- src/services/api-helper.js
+++ src/services/api-helper.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const randomSongs = async (song) => {
-  const response = await axios.get(`https://itunes.apple.com/search?term=${song}&limit=3.`);
+  const response = await axios.get(`https://itunes.apple.com/search?term=${song}&limit=3`);
   const data = response.data.results;
   return data;
 }
+
